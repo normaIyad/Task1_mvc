e610cb54957290da657d09fad6dbbb7c04d360bb
normaIyad <normamohsan39@gmail.com>
2025-05-21 14:21:13 +0300

    Add project files.

---
+++ Task1_mvc/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task1_mvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+        public ViewResult Edit()
+        {
+            return View();
+        }
+    }
+}
